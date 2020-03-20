d2256264097dbbbd03a4500857ee7febb357ac98
Ben Booth <ben@benbooth.dev>
2020-03-21 09:44:03 +1100

    Use live domain for no logo fallback

--- src/utils/getLogoProxy.js
+++ src/utils/getLogoProxy.js
@@ -1,4 +1,4 @@
-export const LOGO_FALLBACK = 'https://icon-preps.netlify.com/images/no-logo.svg';
+export const LOGO_FALLBACK = 'https://iconpreps.com/images/no-logo.svg';
 
 export function getLogoProxy(logoUrl) {
   return `https://images.weserv.nl/?url=${logoUrl}&default=${LOGO_FALLBACK}`;
