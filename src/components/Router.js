73f4c0471535e77d2ba4fef58e62e5dd06cbd3e8
Ben Booth <ben@benbooth.dev>
2020-03-20 23:02:36 +1100

    Scroll page to top on route changes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Redirect, Router as ReachRouter } from '@reach/router';
 import NotFound from 'pages/NotFound';
 import PRepDetail from 'pages/PRepDetail';
@@ -6,15 +6,23 @@ import PRepList from 'pages/PRepList';
 import ProjectDetail from 'pages/ProjectDetail';
 import ProjectList from 'pages/ProjectList';
 
+function ScrollToTop({ children, location }) {
+  useEffect(() => window.scrollTo(0, 0), [location.pathname]);
+
+  return children;
+}
+
 function Router() {
   return (
-    <ReachRouter>
-      <Redirect exact from="/" to="/projects" />
-      <ProjectList path="/projects" />
-      <ProjectDetail path="/projects/:projectId" />
-      <PRepList path="/preps" />
-      <PRepDetail path="/preps/:pRepAddress" />
-      <NotFound default />
+    <ReachRouter primary={false}>
+      <ScrollToTop path="/">
+        <Redirect exact from="/" to="/projects" />
+        <ProjectList path="/projects" />
+        <ProjectDetail path="/projects/:projectId" />
+        <PRepList path="/preps" />
+        <PRepDetail path="/preps/:pRepAddress" />
+        <NotFound default />
+      </ScrollToTop>
     </ReachRouter>
   );
 }
